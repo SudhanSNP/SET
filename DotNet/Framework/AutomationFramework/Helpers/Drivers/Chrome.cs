e95cd88c3445ceedaf40cce866541b1e2b9b1b98
Sudhan9395 <sudhansankar.sankar@gmail.com>
2024-01-15 17:25:36 +0530

    Updated the remote web driver

--- DotNet/Framework/AutomationFramework/Helpers/Drivers/Chrome.cs
+++ DotNet/Framework/AutomationFramework/Helpers/Drivers/Chrome.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 
 namespace Helpers.Drivers
 {
@@ -37,16 +38,13 @@ namespace Helpers.Drivers
 
         public void SetDriver()
         {
-            String driverPath = $"{Directory.GetCurrentDirectory()}\\drivers";
-            String driverExecutableFileName = "chromedriver";
             ChromeOptions chromeOptions = new ChromeOptions();
             chromeOptions.AddArgument("--disable-dev-shm-usage");
             chromeOptions.AddArgument("--no-sandbox");
-            chromeOptions.AddArgument("--headless");
+//            chromeOptions.AddArgument("--headless");
             chromeOptions.AddArgument("ignore-certificate-errors");
-            chromeOptions.AddAdditionalOption("platform", "LINUX");
-            ChromeDriverService service = ChromeDriverService.CreateDefaultService(driverPath, driverExecutableFileName);
-            this.driver = new ChromeDriver(service, chromeOptions);
+            chromeOptions.PlatfornName = "linux";
+            this.driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), chromeOptions)
         }
     }
 }
